61170460849bfdf040bb3314c8270c94c5b93505
Bernat <bernat.grifol@enti.cat>
2020-05-25 11:46:42 +0200

    Prevertical_Recu2

---
+++ Proyecto_Unity_DAMVIOD2_Code/HuntOrBeHunted/Assets/Scripts/Menu.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Menu : MonoBehaviour
+{
+    public GameObject canvasMenu;
+    public GameObject canvasMenuJugar;
+    public GameObject canvasMenuCredit;
+    public GameObject canvasMenuExit;
+    public GameObject canvasWolf;
+    public GameObject canvasDialgue;
+    public GameObject canvasCredits;
+    public GameObject cinemachine;
+    public GameObject wolf;
+
+    private AudioSource ambient;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        canvasMenu.SetActive(true);
+        canvasMenuJugar.SetActive(true);
+        canvasMenuCredit.SetActive(true);
+        canvasMenuExit.SetActive(true);
+        canvasWolf.SetActive(false);
+        canvasDialgue.SetActive(false);
+        canvasCredits.SetActive(false);
+        cinemachine.SetActive(false);
+        wolf.SetActive(false);
+        Time.timeScale = 1;
+        ambient = GetComponent<AudioSource>();
+        ambient.Stop();
+        Cursor.visible = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnClickPlay()
+    {
+        canvasMenu.SetActive(false);
+        canvasWolf.SetActive(true);
+        canvasDialgue.SetActive(true);
+        cinemachine.SetActive(true);
+        wolf.SetActive(true);
+        ambient.Play();
+        Cursor.visible = false;
+    }
+
+    public void RestartGame()
+    {
+        Application.LoadLevel(Application.loadedLevel);
+    }
+
+    public void OnClickCredits()
+    {
+        canvasMenuJugar.SetActive(false);
+        canvasMenuCredit.SetActive(false);
+        canvasMenuExit.SetActive(false);
+        canvasCredits.SetActive(true);
+    }
+
+    public void OnClickExitCredits()
+    {
+        canvasMenuJugar.SetActive(true);
+        canvasMenuCredit.SetActive(true);
+        canvasMenuExit.SetActive(true);
+        canvasCredits.SetActive(false);
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+
+
+}
---
+++ Proyecto_Unity_DAMVIOD2_Code/HuntOrBeHunted/Assets/Scripts/eat.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class eat : MonoBehaviour
+{
+    public TextMeshProUGUI TextMeat;
+    private int food;
+    private int needFood;
+    public GameObject meat;
+
+    public GameObject textHome;
+    public GameObject ImageWine;
+    public GameObject buttonRestartGame;
+
+    private AudioSource sound;
+    public AudioClip comer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        food = 0;
+        needFood = 8;
+        int bucle = 0;
+
+        TextMeat.text = food + " / " + needFood;
+
+        sound = GetComponent<AudioSource>();
+
+        while (needFood > bucle)
+        {
+            Vector3 position = new Vector3(Random.Range(70.0f, 380.0f), 10, Random.Range(150.0f, 400.0f));
+            Instantiate(meat, position, Quaternion.identity);
+            bucle += 1;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(food == needFood)
+        {
+            textHome.SetActive(true);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "meat")
+        {
+            food += 1;
+            TextMeat.text = food + " / " + needFood;
+            sound.clip = comer;
+            sound.Play();
+            Destroy(other.gameObject);            
+        }
+
+        if(food == needFood)
+        {
+            if (other.gameObject.tag == "home")
+            {
+                ImageWine.SetActive(true);
+                buttonRestartGame.SetActive(true);
+                Time.timeScale = 0;
+                Cursor.visible = true;
+            }
+        }
+    }
+}
